37a0b3c30354d6d5d634e49fc749b119f3070b0b
Onur Hüseyin Çantay <ohc3807@gmail.com>
2018-04-28 13:11:24 +0300

    Project init

---
+++ HlsVideoPlayerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HlsVideoPlayerApp
+//
+//  Created by Onur Hüseyin Çantay on 24.04.2018.
+//  Copyright © 2018 Onur Hüseyin Çantay. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
